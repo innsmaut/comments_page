b61843341fa83b27081e036f3db40e90f0d448bb
innsmaut <co3_w4@mail.ru>
2016-06-22 13:14:59 +0300

    etsts

---
+++ deletion.php
@@ -0,0 +1,5 @@
+<?php
+     /*$sql = "DELETE FROM $tablename WHERE id='NULL'";
+     $result = $conn->query($sql);*/
+     echo ("deleted");
+?>
--- index.php
+++ index.php
@@ -14,16 +14,16 @@ if ($baseState == false) {
     <body>
         <div id="commentsBlock">Custom text.
             <p class="comment" id="0"> In the space below you are allowed to place some comments. You can also comment eisting comments up to 5 subcomments.</p>
-            <p class="comment" id="t1">Comments are situated below</p>
-            <p class="comment" id="t2">here is test comment</p>
-            <?php //require 'insertData.php'; ?>
-        <div>
-            <!--<button id="testbutton" onclick="this.parentNode.appendChild(document.getElementById('insertForm'))">Reply</button>-->
-            <button id="testbutton" onclick="forPlusId()">Reply</button>
+        <div id="testbutton">
+            <button onclick="forPlusId()">Reply</button>
+            <form  method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                <input type="hidden" name="delId" value="0" id="delId">
+                <input type="submit" onmouseover="document.getElementsByName('delId')[0].id = document.getElementById('testbutton').parentNode.id" value="Delete">
+                <!--<button onclick="<?php //require 'deletion.php'; ?>">Delete</button>-->
+            </form>
         </div>
-
         </div>
-
+        <div hidden>
         <form  id="insertForm" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
             <fieldset>
                 <legend>Input your name(optional) and new comment</legend>
@@ -35,32 +35,8 @@ if ($baseState == false) {
                 <input type="submit" value="Send" onclick="document.getElementById('1').appendChild(aNC().cloneNode())"><br>
             </fieldset>
         </form>
-
+        </div>
         
-        <script>
-            /*function fitArea() {
-                var getFontSize = 16;
-                document.getElementById("customComment").innerHTML = '';
-                document.getElementById("customComment").style.width = Math.round(window.innerWidth * 0.45).toString() + "px";
-                document.getElementById("customComment").style.height =
-                 (Math.round((500 * getFontSize) / (Math.round(window.innerWidth * 0.45)) + 1) * getFontSize).toString() + "px";
-            }*/
-        </script>
-        <script src="appender.js"></script>
-
-        <script>
-            function test1() {
-                var level = document.getElementById("demo").parentNode.childNodes[1].id;
-                var lMas = level.slice(4, level.length).split(".");
-                for (i = 0; i > 5; i++) {
-                    if (Number(lMas[i]) !== 0 && Number(lMas[i - 1]) !== 0) { lMas[i] = Number(lMas[i]) + 1 }
-                }
-                var levelNew = "base" + lMas.join(".");
-                document.getElementById("demo").innerHTML = "The level is " + level + " and the new id is " + levelNew;
-            }
-        </script>
-
-
         <script>
             function aNC() {
                 <?php 
@@ -86,57 +62,19 @@ if ($baseState == false) {
                 return nodePoster;
             }
         </script>
-            <script>
-            <?php
-                /*$ccom = $_POST['customComment'];
-                $pname = $_POST['posterName'];
-                $piD = $_POST['parentId'];
-                echo "            
-                function postMessage() {
-                document.getElementById('$piD').appendChild(aNC);
-            }";*/
-            ?>
-        </script>
         
-        <p id="commentBase"> 
-            <?php   
-                $pname = $_POST['posterName'];
-                echo "<b>$pname</b>";
-                echo "<br>";
-                $ccom = $_POST['customComment'];
-                echo "$ccom";
-            ?>
-        </p>
-        <div>
-            <button id="testbutton2" onclick="this.parentNode.appendChild(aNC())">TimedReply</button>
-        </div>
 
         <?php
             $posN = $_POST['posterName'];
             $cComm = $_POST['customComment'];
             $piD = $_POST['parentId'];
-            /*$sql = "INSERT INTO $tablename (parid, name, messg) VALUES ('$piD', '$posN', '$cComm');";
+            $sql = "INSERT INTO $tablename (parid, name, messg) VALUES ('$piD', '$posN', '$cComm');";
 
             if ($conn->query($sql) === true) {
             echo "New record created successfully";
             } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
-            }*/
-
-            $sql = "SELECT * FROM $tablename";
-            /*$result = $conn->query($sql)->fetch_assoc();
-            echo $result['name'];
-            if ($result === true) {
-            echo "<script>console.log('Comments are loaded successfully')</script>";
-            } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            }*/
-            $result = $conn->query($sql);
-            while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["id"]. " Parent: " . $row["parid"]. " Name: " . $row["name"]. " Message: " . $row["messg"]. "<br>";
-                }
-
-            echo ('<br>');
+            }
 
             $sql = "SELECT * FROM $tablename ORDER BY parid, id";
             $result = $conn->query($sql);
@@ -148,7 +86,7 @@ if ($baseState == false) {
                 $messg[$y] = $row["messg"];
                 $sid[$y] = $row["id"];
                 echo "
-                <ul id ='$sid[$y]' value='$parid[$y]' class='comment'>Author: $name[$y] Comment: $messg[$y]<br>BLOCK<br>BLOCK<br>BLOCK</ul>
+                <ul id ='$sid[$y]' value='$parid[$y]' class='comment'>Author: $name[$y] Comment: $messg[$y]</ul>
                 <script>
                     function fitall () {
                     document.getElementById($parid[$y]).appendChild(document.getElementById($sid[$y]));
@@ -157,22 +95,12 @@ if ($baseState == false) {
                 </script>
                 ";
                 }
-            /*echo "<script>
-            (function(){
-                for (i=1;i<$len+1;i++){
-                    var paid = document.getElementById(i).getAttribute('value');
-                    console.log(paid);
-                    if (paid!==0){document.getElementById(paid).appendChild(document.getElementById(i));}else{console.log('false')}
-                }
-            })();</script>
-            ";*/
-            
             $conn->close();
         ?>
 
         <script>
             function forPlusId() {
-                document.getElementById("testbutton").parentNode.appendChild(document.getElementById('insertForm'));
+                document.getElementById("testbutton").parentNode.insertBefore(document.getElementById('insertForm'), document.getElementById("testbutton").parentNode.childNodes[1]);
                 var x = document.getElementById("testbutton").parentNode.id;
                 document.getElementById('setParent').value = x;
             }
